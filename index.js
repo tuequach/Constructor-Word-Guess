959f0e7699bc6475f41c2453e3fdb16658257659
tuequach <tuequach@gmail.com>
2019-05-17 19:59:00 -0500

    create reset function with restart or quit option, also shows user whether they have won or not

--- index.js
+++ index.js
@@ -18,16 +18,118 @@ var randomWord = leagueChampions[randomIndex];
 //constructor for leagueChampions array
 compWord = new Word (randomWord);
 
+var newWordNeeded = false; 
+
 var incorrect = [];
 var correct = [];
 
 var guesses = 15; 
 
 function information () {
-    if (newWord) {
+    if (newWordNeeded) {
         var randomIndex = Math.floor(Math.random () * leagueChampions.length);
         var randomWord = leagueChampions[randomIndex];
 
         compWord = new Word (randomWord);
+
+        newWordNeeded = false;
     }
+
+//if letter is guessed correctly
+var wordComplete = [];
+compWord.objArray.forEach(completeCheck);
+
+//letters left that need to be guessed
+if (wordComplete === false) {
+    inquirer
+    .prompt ([
+        {
+            type: 'input',
+            message: 'Guess a letter (A-Z)!',
+            name: 'userInput'
+        }
+    ])
+    .then(function (input) {
+        if (!letterArray === input.userInput || input.userInput.length > 1) {
+            console.log('\nWrong! Try Again!\n');
+            information();
+        } else {
+
+            if (incorrect === input.userInput || correct === input.userInput || input.userInput === '') {
+                console.log("\nYou've Already Guessed It!\n");
+                information();
+            } else {
+                 
+                //Checking if guesses are correct 
+                var checkArray = [];
+
+                compWord.userGuess(input.userInput);
+
+                compWord.objArray.forEach(checkArray);
+                if (checkArray.join('') === wordComplete.join('')) {
+                    console.log('\nThis Is Incorrect!\n');
+
+                    incorrect.push(input.userInput);
+                    guesses--;
+                } else {
+                    console.log ('\nThis is Correct!\n');
+                    correct.push(input.userInput);
+                }
+
+                compWord.log();
+
+//showing gueses left 
+            console.log('Guesses Left: ' + guesses + '\n');
+
+            console.log("Letters That's Been Guessed: " + incorrect.join(' ') + '\n');
+            
+        if (guesses > 0) {
+            information();
+        } else {
+            console.log("Sorry! You've Lost'\n");
+
+            resetGame();
+        }
+
+        function checkArray (key) {
+            checkArray.push(key.guessed);
+                }
+            }
+        }
+    })
+} else {
+    console.log("You WIN!\n");
+
+    resetGame();
 }
+
+function completeCheck(key) {
+    wordComplete.push(key.guessed);
+}
+}
+
+function resetGame() {
+    inquirer
+    .prompt([
+        {
+        type: "list",
+        message:"What Would You Like To Do: ",
+        choices: ['Play Again', 'Quit Game'],
+        name: 'reset'
+        }
+    ])
+    .then (function (input) {
+        if (input.reset === 'Play Again') {
+            newWordNeeded = true;
+            incorrect = [];
+            correct = [];
+            guesses = 15;
+            information();
+        } else {
+            return
+        }
+    })
+}
+
+information();
+
